b236cad951d7472361aa0861e093b62a07551a56
4lonso-dev <gralonso200@gmail.com>
2026-09-22 12:06:01 -0300

    refactor(Votos.services): Limpeza de código e alteração de algumas chamadas pelo Get models de Projeto.

--- services/Votos.services.js
+++ services/Votos.services.js
@@ -10,7 +10,7 @@ export class VotosService{
         const session = await SessionRepository.buscarSession(token, null)
 
         if(!session){
-            throw new Error ('Sessão Invalida!')
+            throw new Error ('Sessão Invalida! Precisa de um Token válido ou existente para votar')
         }
         // Verfica se a session expirou
         const dataHoraHoje = new Date()
@@ -24,34 +24,29 @@ export class VotosService{
         const projeto = await ProjetoRepository.bucarProjetoID(projectId)
 
         if(!projeto){
-            throw new Error ('O projeto não existe!')
+            throw new Error ('O Projeto não existe! Busque por um projeto existente')
         }
 
         const votosCategoria = await VotosRepository.verficarQuantosVotosUserCategoria(
-            session.userIdSession, projeto.categoryIdProjeto
+            session.userIdSession,
+            projeto.categoriaIdProjeto
         )
-        // Verfica quantos 
-        if(votosCategoria.length >= 3){
-            throw new Error ('Limite de votos atingido nessa categoria! O maximo são 3 votos por categoria!')
+        // O maximo por categoria é 1 voto
+        if(votosCategoria >= 1){
+            throw new Error ('Limite de votos atingido nessa categoria! O maximo permitido é 1 voto por categoria!')
         }
 
-        // limite de 1 voto por projeto
-
-        const projetoVotado = votosCategoria.find(voto => voto.projectIdVoto === projectId)
-
-        if(projetoVotado){
-            throw new Error('Você ja votou nesse projeto! Vote em outro projeto!')
-        }
 
         const voto = new Votos(
             null,
             session.userIdSession,
             projeto.idProjeto,
-            projeto.categoryIdProjeto,
+            projeto.categoriaIdProjeto,
             new Date()
         )
 
         const votoFeito = await VotosRepository.salvarVoto(voto)
+        // Ele deleta pois após o voto, ele não vai usar a mesmo token de session
         await SessionRepository.deletarSession(token)
         return votoFeito
     }
